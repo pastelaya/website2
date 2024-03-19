4346b631c7952e0d2c8d1d2174605f17e0ebc41d
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-03-19 22:10:52 +0000

    Tue, Mar 19, 2024, 3:10 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,9 +1,12 @@
 open = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 close = document.getElementById('close-btn')
+var audio = new Audio('sounds/roblox-bye.mp3');
 
+// rules open and close event handlers
 open.addEventListener('click', () => {
     rules.classList.add('show')
+    audio.play(audio);
 })
 
 close.addEventListener('click', () => {
