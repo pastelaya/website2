3ef130b6d8fd6275b489672f41cbfd558e1aa08f
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-03-21 21:36:57 +0000

    Thu, Mar 21, 2024, 2:36 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,11 +1,11 @@
 open = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 close = document.getElementById('close-btn')
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
+// ctx.fillStyle = '#bf9bc9';
+// ctx.fillRect(10, 10, 150, 100);
 
 // rules open and close event handlers
 open.addEventListener('click', () => {
@@ -19,14 +19,20 @@ close.addEventListener('click', () => {
 // ball properties
 ball = {
     x: canvas.width / 2,
-    y: cancas.height / 2,
+    y: canvas.height / 2,
     size: 10,
     speed: 4,
-    dx: 4;
-    dy: -4;
+    dx: 4,
+    dy: -4,
 }
 
 // draw ball on canvas
 function drawBall() {
     ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#bf9bc9'
+    ctx.fill()
+    ctx.closePath()
 }
+
+drawBall()
