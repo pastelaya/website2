d5f826ffe5ef66b8a90ee0e7423b180682bf3606
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-03-21 21:29:22 +0000

    Thu, Mar 21, 2024, 2:29 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -2,9 +2,26 @@ open = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 close = document.getElementById('close-btn')
 var audio = new Audio('sounds/roblox-bye.mp3');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
 
-ball =
+// ball properties
+ball = {
+    x: canvas.width / 2,
+    y: cancas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+}
+
+// draw ball on canvas
+function drawBall() {
+
+}
 
 // rules open and close event handlers
 open.addEventListener('click', () => {
