522d669e4d6661776e9b916feeaae26e5f906acf
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-04-02 21:57:13 +0000

    Tue, Apr 2, 2024, 2:57 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -161,123 +161,19 @@ if (ball.x + ball.size < 0)
 // paddle collision
 if (ball.x - ball.size > paddle.x && paddle.x + ball.size < paddle.x + paddle.w && ball.y + ball.size < paddle.y)
 {
-    ball.dy = -1 * ball.dy]
-
-]
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]][\[\[[[[[[[[[[[[[\[\
-
-]]]]]]]]]]]]]]]]
+    ball.dy = -1 * ball.dy
+}
+
+/// brick collision
+bricks.forEach(column => {
+    column.forEach(brick => {
+        if (brick.visible) {
+            if (ball.y - ball.size < brick.y + brick.h)
+            ball.dy = -1 * ball.dy
+            brick.visible = false
+            }
+        })
+    })
 }
 
 }
