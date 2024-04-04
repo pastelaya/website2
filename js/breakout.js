55f8c44beb9c3d67d911c4ed359035fbdb23e3e0
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-04-04 22:00:54 +0000

    Thu, Apr 4, 2024, 3:00 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -84,7 +84,7 @@ function drawBricks() {
         column.forEach(brick => {
             ctx.beginPath()
             ctx.rect(brick.x, brick.y, brick.w, brick.h)
-            ctx.fillStyle = brick.visible ? '#bf9bc9' : 'transparent';
+            ctx.fillStyle = brick.visible ? '#bf9bc9' : 'transparent'
             ctx.fill()
             ctx.closePath()
         })
