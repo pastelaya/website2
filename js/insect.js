c81e88ef60e4bdb5cb0ef67235a936bfa91da140
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-04-11 21:30:14 +0000

    Thu, Apr 11, 2024, 2:30 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -84,4 +84,5 @@ function getRandomLocation() {
     const height = window.innerHeight
     const x = Math.random() * (width - 200) + 100
     const y = Math.random() * (height - 200) + 100
+    return {x, y}
 }
