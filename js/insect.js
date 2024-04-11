cb7eae7b2da4636324c2edea0587a3d06333d96e
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-04-11 21:02:37 +0000

    Thu, Apr 11, 2024, 2:02 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -31,6 +31,9 @@ function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
     const { x, y } = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="selected_insect.src" alt = "selected_insect.alt">`
 }
 
 
