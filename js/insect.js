6a66641400de4d23f31fdeb0c3758b72b2cc6136
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-04-11 20:37:05 +0000

    Thu, Apr 11, 2024, 1:37 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -5,5 +5,8 @@ const start_btn = document.getElementById('start-btn')
 const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
 
