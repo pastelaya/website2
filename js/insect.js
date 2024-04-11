69f1da0117dd72e629d00a2af9abc5f534467031
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-04-11 21:25:59 +0000

    Thu, Apr 11, 2024, 2:25 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -34,13 +34,21 @@ function startGame() {
 function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
+    if (m < 10) {
+        m = `0${m}`
+    }
+
+    if (s < 10) {
+        s = `0${s}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
 }
 
 
 function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
-    const {x, y} = getRandomLocation()
+    const { x, y } = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
     insect.innerHTML = `<img src="${selected_insect.src}" alt = "${selected_insect.alt}" style = "transform: rotate(${Math.random() * 360}deg)" />`
