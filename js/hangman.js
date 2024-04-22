cee3988fb875ac3b60cb9c2dc4b6bfb56794b027
pastelaya <142944386+pastelaya@users.noreply.github.com>
2024-04-22 21:37:22 +0000

    Mon, Apr 22, 2024, 2:37 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -3,3 +3,23 @@ wrongLettersEl = document.getElementById('wrong-letters')
 const playAgainBtn = document.getElementById('play-again')
 const popup = document.getElementById('pupop-container')
 const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['rogue', 'barbarian', 'bard', 'fighter', 'cleric', 'paladin']
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[selectedIndex]
+
+const correctLetters = []
+const wrongLetters = []
+
+// show hidden word
+function displayWord() {
+    wordEl.innerHTML = `
+    ${selectedWord}
+
+    `
+}
+
+displayWord()
